e85d8acee64deb8a8b7e1755597961fc87dd57cf
MATIN <mtdrfsh@gmail.com>
2021-01-14 21:15:05 +0330

    delete comment

--- src/dsproject.java
+++ src/dsproject.java
@@ -1,8 +1,7 @@
 public class dsproject {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        //this is a test from Matin
-		//pedram test
+
         
     }
 }
