fd5a4aa7b1db21b5a4a47b7295af279d15358cf6
Pedram <eng.pedram.r@gmail.com>
2021-01-14 21:12:54 +0330

    delete lines

--- src/dsproject.java
+++ src/dsproject.java
@@ -1,11 +1,8 @@
 public class dsproject {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< HEAD
         //this is a test from Matin
-=======
 		//pedram test
         
->>>>>>> e7c7ca9805353801be6552e9f2c23e6bd3ac2d2f
     }
 }
