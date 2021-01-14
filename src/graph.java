9412d30bbf10dc84c1376614a16bf4961a1224e4
Pedram <eng.pedram.r@gmail.com>
2021-01-15 00:22:44 +0330

    vertex and edge java classes added

--- src/graph.java
+++ src/graph.java
@@ -1,6 +1,26 @@
 import java.util.HashMap;
 
 public class graph {
-    HashMap <String,String> vertex=new HashMap<String, String>();
-    HashMap <String,String> edges=new HashMap<String, String>();
+    HashMap <String,vertex> vertices;
+    int vnums=0;
+    HashMap <String,edge> edges;
+    int enums=0;
+    public graph(){
+        vertices=new HashMap<String, vertex>();
+        edges=new HashMap<String, edge>();
+        vnums=0;
+        enums=0;
+    }
+    public int vertex_numbers(){
+        return vertices.size();
+    }
+    public int edge_numbers(){
+        return edges.size();
+    }
+    private void addvertex(String key,vertex v){
+        if(vertices.putIfAbsent(key,v).equals("Yes"))
+            System.out.println("Same vertex exist !");
+        else
+            vnums++;
+    }
 }
