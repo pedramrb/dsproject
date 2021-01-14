43629640bb835d7ac76baaeee13dd782cd9c09a8
Pedram <eng.pedram.r@gmail.com>
2021-01-14 23:43:38 +0330

    gh

--- src/dsproject.java
+++ src/dsproject.java
@@ -1,23 +1,16 @@
+import java.util.HashMap;
+
 public class dsproject {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-<<<<<<< HEAD
-<<<<<<< HEAD
+        //HashMap<Integer,String> map=new HashMap<Integer,String>();//Creating HashMap
+        HashMap <String,String> map=new HashMap<String, String>();
+        System.out.println(map.put("154","Yes"));
+        System.out.println(map.putIfAbsent(" 154","No"));
+        System.out.println(map.clone());
 
-        
-=======
-        //this is a test from Matin
-		//pedram test
-        //test for branch pedram
->>>>>>> a7a263f8a6c8dbd96c72b8e389843643bc7f6cd6
-=======
-        //this is a test from Matin
-		//pedram test
-        //test for branch pedram
-=======
+        System.out.println(map.put(" 154","No"));
+        System.out.println(map.clone());
 
-        
->>>>>>> e85d8acee64deb8a8b7e1755597961fc87dd57cf
->>>>>>> pedram
+        System.out.println(map.size());
     }
 }
--- src/graph.java
+++ src/graph.java
@@ -1,2 +1,6 @@
+import java.util.HashMap;
+
 public class graph {
+    HashMap <String,String> vertex=new HashMap<String, String>();
+    HashMap <String,String> edges=new HashMap<String, String>();
 }
