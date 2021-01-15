ee47b0cabb1be08129bc0a92f821aecd3db7d5ae
Pedram <eng.pedram.r@gmail.com>
2021-01-15 15:18:26 +0330

    a

--- src/phase_one.java
+++ src/phase_one.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class phase_one {
     public static void main(String[] args) {
         //Scanner scan=new Scanner(new File("dateSample"))
-        File f=new File("../dataSample");
+        File f=new File("dataSample");
         String ap=f.getAbsolutePath();
         File []subfiles=f.getAbsoluteFile().listFiles();
+        System.out.println(f.getAbsolutePath());
         for(File x:subfiles){
             System.out.print(x.getPath()+"\t");
             System.out.println(x.getName().replaceAll(".csv",""));
