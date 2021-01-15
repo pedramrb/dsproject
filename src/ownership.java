cefcb4107e01c7cfea5df2dc459c38c0ed771b8c
MATIN <mtdrfsh@gmail.com>
2021-01-15 14:34:08 +0330

    fix edge

--- src/ownership.java
+++ src/ownership.java
@@ -3,10 +3,7 @@ public class ownership extends edge {
     String date;
     String amount;
     public ownership(String key,String value,vertex from,vertex to,String ownershipid,String date,String amount){
-        this.key=key;
-        this.value=value;
-        this.from=from;
-        this.to=to;
+        super(key,value,from,to);
         this.ownershipid=ownershipid;
         this.date=date;
         this.amount=amount;
--- src/relationship.java
+++ src/relationship.java
@@ -2,10 +2,7 @@ public class relationship extends edge{
     String relation;
     String date;
     public relationship(String key,String value,vertex from,vertex to,String relation,String date){
-        this.key=key;
-        this.value=value;
-        this.from=from;
-        this.to=to;
+        super(key,value,from,to);
         this.relation=relation;
         this.date=date;
     }
