a9e9792a74f86e2ba37a4f6ab07249c78842328f
MATIN <mtdrfsh@gmail.com>
2021-01-15 15:23:57 +0330

    fix

--- src/phase_one.java
+++ src/phase_one.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 
 public class phase_one {
     public static void main(String[] args) {
-        //Scanner scan=new Scanner(new File("dateSample"))
         File f=new File("dataSample");
-        String ap=f.getAbsolutePath();
         File []subfiles=f.getAbsoluteFile().listFiles();
-        System.out.println(f.getAbsolutePath());
         for(File x:subfiles){
             System.out.print(x.getPath()+"\t");
             System.out.println(x.getName().replaceAll(".csv",""));
